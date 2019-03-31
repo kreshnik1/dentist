5e9a64d28ea38971be84ebf6eb41de6e7d97f8fc
kreshnik1 <kreshnik72@gmail.com>
2019-04-01 01:20:25 +0200

    add

--- public/js/validation.js
+++ public/js/validation.js
@@ -216,68 +216,53 @@ if(document.querySelector(".navbar-toggler")){
 }
 
 let color1 = '#ffa4a2';
+
+for(let i=11 ; i <= 18 ; i++){
+  $('.tooth-'+i).mouseleave(function(){
+    $('.tooth-'+i+'-parent').css('fill', 'none');
+  });
+}
+for(let i=21 ; i <= 28 ; i++){
+  $('.tooth-'+i).mouseleave(function(){
+    $('.tooth-'+i+'-parent').css('fill', 'none');
+  });
+}
+for(let i=31 ; i <= 38 ; i++){
+  $('.tooth-'+i).mouseleave(function(){
+    $('.tooth-'+i+'-parent').css('fill', 'none');
+  });
+}
+for(let i=41 ; i <= 48 ; i++){
+  $('.tooth-'+i).mouseleave(function(){
+    $('.tooth-'+i+'-parent').css('fill', 'none');
+  });
+}
+
+
 $('.tooth-11').mouseover(function(){
   $('.tooth-11-parent').css('fill', color1);
-});
-
-$('.tooth-11').mouseleave(function(){
-  $('.tooth-11-parent').css('fill', 'none');
 });
 
 $('.tooth-12').mouseover(function(){
   $('.tooth-12-parent').css('fill', color1);
 });
-
-$('.tooth-12').mouseleave(function(){
-  $('.tooth-12-parent').css('fill', 'none');
-});
-
 $('.tooth-13').mouseover(function(){
   $('.tooth-13-parent').css('fill', color1);
 });
-
-$('.tooth-13').mouseleave(function(){
-  $('.tooth-13-parent').css('fill', 'none');
-});
-
 $('.tooth-14').mouseover(function(){
   $('.tooth-14-parent').css('fill', color1);
-});
-
-$('.tooth-14').mouseleave(function(){
-  $('.tooth-14-parent').css('fill', 'none');
 });
 
 $('.tooth-15').mouseover(function(){
   $('.tooth-15-parent').css('fill', color1);
 });
 
-$('.tooth-15').mouseleave(function(){
-  $('.tooth-15-parent').css('fill', 'none');
-});
-
 $('.tooth-16').mouseover(function(){
   $('.tooth-16-parent').css('fill', color1);
 });
 
-$('.tooth-16').mouseleave(function(){
-  $('.tooth-16-parent').css('fill', 'none');
-});
-
-$('.tooth-17').mouseover(function(){
-  $('.tooth-17-parent').css('fill', color1);
-});
-
-$('.tooth-17').mouseleave(function(){
-  $('.tooth-17-parent').css('fill', 'none');
-});
-
 $('.tooth-18').mouseover(function(){
   $('.tooth-18-parent').css('fill', color1);
-});
-
-$('.tooth-18').mouseleave(function(){
-  $('.tooth-18-parent').css('fill', 'none');
 });
 
 /*Segundo Quadrante*/
@@ -286,65 +271,45 @@ $('.tooth-21').mouseover(function(){
   $('.tooth-21-parent').css('fill', color1);
 });
 
-$('.tooth-21').mouseleave(function(){
-  $('.tooth-21-parent').css('fill', 'none');
-});
 
 $('.tooth-22').mouseover(function(){
   $('.tooth-22-parent').css('fill', color1);
 });
 
-$('.tooth-22').mouseleave(function(){
-  $('.tooth-22-parent').css('fill', 'none');
-});
 
 $('.tooth-23').mouseover(function(){
   $('.tooth-23-parent').css('fill', color1);
-});
-
-$('.tooth-23').mouseleave(function(){
-  $('.tooth-23-parent').css('fill', 'none');
 });
 
 $('.tooth-24').mouseover(function(){
   $('.tooth-24-parent').css('fill', color1);
 });
 
-$('.tooth-24').mouseleave(function(){
-  $('.tooth-24-parent').css('fill', 'none');
-});
+
 
 $('.tooth-25').mouseover(function(){
   $('.tooth-25-parent').css('fill', color1);
 });
 
-$('.tooth-25').mouseleave(function(){
-  $('.tooth-25-parent').css('fill', 'none');
-});
+
 
 $('.tooth-26').mouseover(function(){
   $('.tooth-26-parent').css('fill', color1);
 });
 
-$('.tooth-26').mouseleave(function(){
-  $('.tooth-26-parent').css('fill', 'none');
-});
+
 
 $('.tooth-27').mouseover(function(){
   $('.tooth-27-parent').css('fill', color1);
 });
 
-$('.tooth-27').mouseleave(function(){
-  $('.tooth-27-parent').css('fill', 'none');
-});
+
 
 $('.tooth-28').mouseover(function(){
   $('.tooth-28-parent').css('fill', color1);
 });
 
-$('.tooth-28').mouseleave(function(){
-  $('.tooth-28-parent').css('fill', 'none');
-});
+
 
 /*Terceiro Quadrante*/
 
@@ -352,65 +317,44 @@ $('.tooth-31').mouseover(function(){
   $('.tooth-31-parent').css('fill', color1);
 });
 
-$('.tooth-31').mouseleave(function(){
-  $('.tooth-31-parent').css('fill', 'none');
-});
 
 $('.tooth-32').mouseover(function(){
   $('.tooth-32-parent').css('fill', color1);
 });
 
-$('.tooth-32').mouseleave(function(){
-  $('.tooth-32-parent').css('fill', 'none');
-});
+
 
 $('.tooth-33').mouseover(function(){
   $('.tooth-33-parent').css('fill', color1);
 });
 
-$('.tooth-33').mouseleave(function(){
-  $('.tooth-33-parent').css('fill', 'none');
-});
 
 $('.tooth-34').mouseover(function(){
   $('.tooth-34-parent').css('fill', color1);
 });
 
-$('.tooth-34').mouseleave(function(){
-  $('.tooth-34-parent').css('fill', 'none');
-});
+
 
 $('.tooth-35').mouseover(function(){
   $('.tooth-35-parent').css('fill', color1);
 });
 
-$('.tooth-35').mouseleave(function(){
-  $('.tooth-35-parent').css('fill', 'none');
-});
 
 $('.tooth-36').mouseover(function(){
   $('.tooth-36-parent').css('fill', color1);
 });
 
-$('.tooth-36').mouseleave(function(){
-  $('.tooth-36-parent').css('fill', 'none');
-});
 
 $('.tooth-37').mouseover(function(){
   $('.tooth-37-parent').css('fill', color1);
 });
 
-$('.tooth-37').mouseleave(function(){
-  $('.tooth-37-parent').css('fill', 'none');
-});
+
 
 $('.tooth-38').mouseover(function(){
   $('.tooth-38-parent').css('fill', color1);
 });
 
-$('.tooth-38').mouseleave(function(){
-  $('.tooth-38-parent').css('fill', 'none');
-});
 
 /*Quarto Quadrante*/
 
@@ -418,65 +362,40 @@ $('.tooth-41').mouseover(function(){
   $('.tooth-41-parent').css('fill',color1);
 });
 
-$('.tooth-41').mouseleave(function(){
-  $('.tooth-41-parent').css('fill', 'none');
-});
+
 
 $('.tooth-42').mouseover(function(){
   $('.tooth-42-parent').css('fill', color1);
-});
-
-$('.tooth-42').mouseleave(function(){
-  $('.tooth-42-parent').css('fill', 'none');
 });
 
 $('.tooth-43').mouseover(function(){
   $('.tooth-43-parent').css('fill', color1);
 });
 
-$('.tooth-43').mouseleave(function(){
-  $('.tooth-43-parent').css('fill', 'none');
-});
+
 
 $('.tooth-44').mouseover(function(){
   $('.tooth-44-parent').css('fill', color1);
-});
-
-$('.tooth-44').mouseleave(function(){
-  $('.tooth-44-parent').css('fill', 'none');
 });
 
 $('.tooth-45').mouseover(function(){
   $('.tooth-45-parent').css('fill', color1);
 });
 
-$('.tooth-45').mouseleave(function(){
-  $('.tooth-45-parent').css('fill', 'none');
-});
 
 $('.tooth-46').mouseover(function(){
   $('.tooth-46-parent').css('fill', color1);
 });
 
-$('.tooth-46').mouseleave(function(){
-  $('.tooth-46-parent').css('fill', 'none');
-});
 
 $('.tooth-47').mouseover(function(){
   $('.tooth-47-parent').css('fill', color1);
 });
-
-$('.tooth-47').mouseleave(function(){
-  $('.tooth-47-parent').css('fill', 'none');
-});
-
 $('.tooth-48').mouseover(function(){
   $('.tooth-48-parent').css('fill', color1);
 });
 
-$('.tooth-48').mouseleave(function(){
-  $('.tooth-48-parent').css('fill', 'none');
-});
+
 
 
 $('#exampleModal').on('show.bs.modal', function (event) {
@@ -629,12 +548,6 @@ if(document.querySelector('.type')){
   })
 }
 */
-
-$('#modal-view-event').on('hidden.bs.modal', function () {
-  document.querySelector("input[name='name']").value="";
-  document.querySelector("input[name='surname']").value="";
-  document.querySelector("input[name='password']").value="";
-});
 
 
 if(document.querySelector("#protezaSiper11")){
